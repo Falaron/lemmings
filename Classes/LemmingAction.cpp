4d40adb56d57f913176e82c193ad26fb648d84dd
Hugo.M <hmaestracci@gaming.tech>
2023-01-05 17:46:56 +0100

    fix actionCounter pos

--- Classes/LemmingAction.cpp
+++ Classes/LemmingAction.cpp
@@ -17,9 +17,9 @@ void LemmingAction::Initialize(int index, LemmingActionName action, int actionCo
 	setPositionX(40 + (index * getContentSize().width * getScale()));
 	setAnchorPoint(Vec2(0, 0));
 
-	_actionCountText = Label::createWithSystemFont(std::to_string(actionCount), "fonts/arial.ttf", 15);
-	_actionCountText->setAnchorPoint(Vec2(0.5, 0.5));
-	_actionCountText->setPosition(5, 30);
+	_actionCountText = Label::createWithSystemFont(std::to_string(actionCount), "fonts/arial.ttf", 9);
+	_actionCountText->setPositionX(16 * ACTION_SPRITE_SCALE_FACTOR);
+	_actionCountText->setPositionY(_actionCountText->getPositionX());
 	addChild(_actionCountText);
 
 	//TODO: move counter small
